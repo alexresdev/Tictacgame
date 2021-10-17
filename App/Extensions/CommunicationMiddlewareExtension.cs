4748e8bff03c16bc49b44d32b4d984cca3b91570
alexresdev <alexresdev@gmail.com>
2021-10-17 19:09:29 +0300

    Skeleton

---
+++ App/Extensions/CommunicationMiddlewareExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Application.Middleware;
+
+namespace Application.Extensions
+{
+    public static class CommunicationMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCommunicationMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CommunicationMiddleware>();
+        }
+    }
+}
--- App/Startup.cs
+++ App/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Application.Services;
+using Application.Extensions;
+using Application.Models;
 
 namespace Application
 {
@@ -19,7 +21,10 @@ namespace Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+            //services.AddDirectoryBrowser();
             services.AddSingleton<IUserService, UserService>();
+            services.AddRouting();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  // IHostingEnvironment is obsolete
         {
@@ -31,14 +36,46 @@ namespace Application
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseStaticFiles();
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("CreateUser", context =>
+            {
+                var firstName = context.Request.Query["firstName"];
+                var lastName = context.Request.Query["lastName"];
+                var email = context.Request.Query["email"];
+                var password = context.Request.Query["password"];
+                var userService = context.RequestServices.
+                GetService<IUserService>();
+                userService.RegisterUser(new UserModel
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    Password = password
+                });
+                return context.Response.WriteAsync($"User {firstName} {lastName} has been successfully created.");
+            });
+
+            app.UseCommunicationMiddleware();
+
             app.UseRouting();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
+
+
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status Code: {0}");
         }
+
     }
 }
